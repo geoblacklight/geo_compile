1fe59f68d6063002ccf2785a2f1fb652e2e8ef20
Stephen Balogh <sgbalogh@gmail.com>
2017-08-04 17:25:33 -0400

    starts translate

--- lib/geo_compile/format_translators/gbl1_0_to_gbl2_0.rb
+++ lib/geo_compile/format_translators/gbl1_0_to_gbl2_0.rb
@@ -6,6 +6,37 @@ module GeoCompile
       end
 
       def translate(metadata); end
+
+      private
+
+      ##
+      # Create dcat:distribution object for a download
+      # @param [String,String,String,String,String]
+      # @return [Hash]
+      def generate_distribution_download(title,format,media_type,value,conforms_to = nil)
+        {
+            :@type => 'dcat:Distribution',
+            conformsTo: conforms_to,
+            downloadURL: value,
+            format: format,
+            mediaType: media_type,
+            title: title
+        }.delete_if { |k,v| v.nil? }
+      end
+
+      ##
+      # Create dcat:distribution object for a web service
+      # @param [String,String,String,String,String]
+      # @return [Hash]
+      def generate_distribution_service(value,conforms_to,layer_id = nil,downloadable_as = nil)
+        {
+            :@type => 'dcat:Distribution',
+            accessURL: value,
+            conformsTo: conforms_to,
+            downloadableAs: downloadable_as,
+            layerId: layer_id
+        }.delete_if { |k,v| v.nil? }
+      end
     end
   end
 end
